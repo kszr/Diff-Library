f1973b1a28af702e0f92bb03c3ba75b35b52e4e3
Abhishek Chatterjee <achatterjee32@gatech.edu>
2016-02-14 19:11:50 -0500

    Updates

--- diffchecker.py
+++ diffchecker.py
@@ -1,8 +1,13 @@
 """
 Different ways of measuring diffs between files.
+
+Be sure to have bsdiff installed: http://starship.python.net/crew/atuining/cx_bsdiff/index.html
 """
 
+import argparse
+import bsdiff
 from difflib import SequenceMatcher
+import os
 import sys
 
 """
@@ -13,24 +18,40 @@ of text or whatever, meaning this might just turn out to be more like the "effic
 than previously thought.
 From: http://stackoverflow.com/a/1334758
 """
-def binaryDiff(file1, file2, function="normal"):
+def binaryDiff(file1, file2, speed="normal"):
     try:
         text1 = open(file1).read()
         text2 = open(file2).read()
         m = SequenceMatcher(None, text1, text2)
-        # return m.ratio()
+
         function_dict = {"normal" : m.ratio,
                         "quick" : m.quick_ratio}
-        return function_dict[function]()
+        return function_dict[speed]()
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+def binaryDiff2(file1, file2):
+    try:
+        text1 = open(file1).read()
+        text2 = open(file2).read()
+        
+        return 2.0*len(bsdiff.Diff(text1, text2)[1])/(len(text1) + len(text2))
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
 """
-@TODO: Implement this.
-What is this supposed to do anyway?
+@TODO: Implement this, maybe.
 """
-def efficientDiff(file1, file2):
+def efficientDiff(file1, file2, speed="normal"):
     return None
+
+def argument_init():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("experiment", default="binary", choices=["binary", "binary2", "efficient"], help="A experiment scenario to run")
+    parser.add_argument("-q", "--quick", action="store_true", help="A quick estimate of differences")
+    parser.add_argument("-f", "--files", nargs=2, required=True, help="The two files to compare")
+
+    return parser.parse_args()
 
 """
 This will probably never be implemented, because it has different
@@ -41,30 +62,23 @@ def logicalDiff(file1, file2):
     return None
 
 def main():
-    if(len(sys.argv) > 5 or len(sys.argv) < 3):
-        print "Usage: python diffchecker.py [file1] [file2] ([binaryDiff | efficientDiff]) ([normal | quick])"
-        exit(1)
+    args = argument_init()
 
-    file1 = str(sys.argv[1])
-    file2 = str(sys.argv[2])
-    diffMethod = binaryDiff
-    speed = "normal"
+    experiment = args.experiment
+    quick = args.quick
+    files = args.files
 
-    function_dict = {"binaryDiff" : binaryDiff,
-                     "efficientDiff" : efficientDiff}
-    if len(sys.argv) >= 4:
-        diffMethodString = sys.argv[3]
-        try:
-            diffMethod = function_dict[diffMethodString]
-            if len(sys.argv) == 5:
-                speed = sys.argv[4]
-        except KeyError:
-            diffMethod = binaryDiff
-            speed = sys.argv[3]
+    function_dict = {'binary' : binaryDiff,
+                     'binary2': binaryDiff2,
+                     'efficient' : efficientDiff}
+    diffMethod = function_dict[experiment]
 
-    print diffMethod(file1, file2, speed)
+    if quick:
+        print diffMethod(files[0], files[1], "quick")
+    else:
+        print diffMethod(files[0], files[1])
 
+    print "File 1 size:", str(os.stat(files[0]).st_size), "B\nFile 2 size:", str(os.stat(files[1]).st_size), "B"
 
 if __name__ == "__main__":
     main()
-
